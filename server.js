e3ddf78702c8f6e69aad7746b8b372ab804e6df5
William Rave <wrave@gwu.edu>
2020-05-07 18:23:58 -0400

    Fixed order of routes.

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const PORT = 3000;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,"index.html"));
-});
-
 app.get("/notes",function(req,res){
     res.sendFile(path.join(__dirname,"notes.html"));
 });
 
-/*app.get("/api/notes",function(res,req){
-    res.json(path.join(__dirname,"/db/db.json"));
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
 });
 
-app.post("/api/notes",function(res,req){
+/*app.get("/api/notes",function(req,res){
+    console.log(path.join(__dirname,"db/db.json"));
+    res.json(path.join(__dirname,"db/db.json"));
+});
+
+app.post("/api/notes",function(req,res){
 
 });*/
 

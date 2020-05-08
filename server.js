a55a677f3ccc75436bee5cf66ee9282b580745c7
William Rave <wrave@gwu.edu>
2020-05-07 21:28:07 -0400

    Delete function written, but non-functional

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/notes",function(req,res){
 });
 
 app.get("/api/notes",function(req,res){
-    res.json(processJSON);
+    res.json(getJSON);
 });
 
 app.get("*",function(req,res){
@@ -20,20 +20,37 @@ app.get("*",function(req,res){
 });
 
 app.post("/api/notes",function(req,res){
-    let allNotes = processJSON();
+    let allNotes = getJSON();
     let newID = determineID(allNotes);
     let newNote = {title: req.body.title, text: req.body.text, id: newID};
     allNotes.push(newNote);
     allNotes = JSON.stringify(allNotes);
     fs.writeFileSync("./db/db.json",allNotes);
-    res.end();
+    res.sendFile(path.join(__dirname,"notes.html"));
+});
+
+app.delete("/api/notes/:id",function(req,res)
+{
+    let allNotes = getJSON();
+    let newNotes = [];
+    let id = req.params.id;
+
+    allNotes.forEach(targetNote => {
+        if(targetNote.id !== id)
+        {
+            newNotes.push(targetNote);
+        }
+    });
+    newNotes = JSON.stringify(newNotes);
+    fs.writeFileSync("./db/db.json",newNotes);
+    res.sendFile(path.join(__dirname,"notes.html"));    
 });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
   
-function processJSON()
+function getJSON()
 {
     let allNotes = fs.readFileSync("./db/db.json");
     allNotes = JSON.parse(allNotes);
